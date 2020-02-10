c0968db33e7f969d02f5872faebdb555a27cdb87
ilkhomkhafizov <ilhomhafizov6@gmail.ru>
2020-02-10 05:55:21 +0300

    create sign-up component, and implemented firebase utils from sign-up

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-import "./sign-in-and-sign-up.sass";
 import SignIn from "./../../components/sign-in/sign-in.component";
+import SignUp from "./../../components/sign-up/sign-up.component";
+import "./sign-in-and-sign-up.sass";
 
 const SignInAndSignUp = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
